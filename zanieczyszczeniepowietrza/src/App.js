da876b88c9d9e91475b08490d30278a416b896c3
niesyto <jarekniesyto@gmail.com>
2019-10-08 04:35:17 +0200

    GPS data from device

--- zanieczyszczeniepowietrza/src/App.js
+++ zanieczyszczeniepowietrza/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import MenuBar from './MenuBar.js';
+import TabPanel from './TabPanel.js';
+import NavigationPanel from './NavigationPanel.js';
 import Typography from '@material-ui/core/Typography';
 
-import gif from './GPS.gif'
 
 function App() {
   const [mode, setMode] = React.useState(0);
+  const [crd, setCrd] = React.useState(0);
+
+
+  function success(pos) {
+    setCrd(pos.coords);
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success,error);
+
 
   return (
     <>
@@ -14,11 +34,19 @@ function App() {
         setMode={setMode}
       />
 
+      <TabPanel value={mode} index={0}>
+        <NavigationPanel coords={crd}/>
+      </TabPanel>
+      <TabPanel value={mode} index={1}>
+          {mode}
+      </TabPanel>    
+
+     
       <Typography color='secondary'>
         example project
       </Typography>
 
-      <img src={gif} alt="Gif with a satelite"/>
+      
     </>
   );
 }
